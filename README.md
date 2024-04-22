a5dd747c98beee2c2f549ad7311a72df89bd5548
JoshCu <josh.cu@gmail.com>
2024-04-21 22:35:20 -0500

    add more comments

--- README.md
+++ README.md
@@ -10,9 +10,9 @@ This project includes Python scripts to find the maximal clique in a graph using
 
 1. [`brute_force.py`](brute_force.py): This script uses a simple brute force algorithm to find the maximal clique. It is slow and inefficient, but guaranteed to work.
 
-2. [`vertex_cover.py`](vertex_cover.py): This script will find the vertex cover of the compliment graph, then use that to give an approximation of the max clique
+2. [`vertex_cover.py`](vertex_cover.py): This script will find the vertex cover of the complement graph, then use that to give an approximation of the max clique
 
-3. [`comp.py`](comp.py): This script uses a more efficient algorithm to find the maximal clique. It's similar to the vertex_cover implementation except it uses a better vertex cover algorithm. It uses the `numba` library for just-in-time compilation to speed up the computation.
+3. [`comp.py`](comp.py): This script uses a more efficient algorithm to find the maximal clique. It still works by subtracting the vertex cover of the complement from the nodes, but it calculates the vertex cover by adding the highest degree nodes first. It uses the `numba` library for just-in-time compilation to speed up the computation.
 
 4. [`utils.py`](utils.py): This script includes utility functions to load the adjacency matrix from a file and parse command line arguments.
 
--- comp.py
+++ comp.py
@@ -1,3 +1,10 @@
+# Similar to vertex_cover.py
+# This script finds an approximation of the maximal clique in a graph
+# First find the complement of the graph
+# Then find the vertex cover of the complement
+######### adding the node with the highest degree first! #########
+# The maximal clique is the set of all nodes not in the complement vertex cover
+
 from utils import load_file, parse_args
 import numpy as np
 from numba import njit
@@ -11,21 +18,31 @@ def complement_graph(graph):
 def greedy_vertex_cover(graph):
     n = len(graph)
     cover = set()
+    # the number of edges in the graph
+    # is the sum of the upper triangle of the adjacency matrix
     remaining_edges = np.sum(graph) // 2
     max_edges = remaining_edges
+    # while there are still edges in the graph
     while remaining_edges > 0:
         max_degree = -1
         max_degree_vertex = -1
-
+        # find the node with the highest degree
         for i in range(n):
+            # if the node is not already in the cover
             if i not in cover:
                 degree = np.sum(graph[i])
+                # check if the degree is higher than the current max
+                # and if it is, update the max
                 if degree > max_degree:
                     max_degree = degree
                     max_degree_vertex = i
 
+        # add the node with the highest degree to the cover
         cover.add(max_degree_vertex)
+        # remove the edges connected to the node
+        # this sets the row to all zeros
         graph[max_degree_vertex] = np.zeros(n)
+        # this magic numpy slice sets the column to all zeros
         graph[:, max_degree_vertex] = 0
         remaining_edges -= max_degree
 
